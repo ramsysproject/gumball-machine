7987ede92c479424ab24283bd450367b4143ba4b
emanuelramirez <emanuel.ramirez@bestseller.com>
2019-06-18 18:04:40 +0200

    Gumball machine implementation

--- src/main/java/com/emramirez/gumballmachine/GumballMachineApplication.java
+++ src/main/java/com/emramirez/gumballmachine/GumballMachineApplication.java
@@ -1,5 +1,6 @@
 package com.emramirez.gumballmachine;
 
+import com.emramirez.gumballmachine.domain.GumballMachine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,39 @@ public class GumballMachineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GumballMachineApplication.class, args);
+
+		GumballMachine gumballMachine = new GumballMachine(5);
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrack();
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.ejectQuarter();
+		gumballMachine.turnCrack();
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrack();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrack();
+		gumballMachine.ejectQuarter();
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrack();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrack();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrack();
+
+		System.out.println(gumballMachine);
 	}
 
 }
