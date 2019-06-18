44b081a724ffcbb7211f7ad0ab96e0fe53395533
emanuelramirez <emanuel.ramirez@bestseller.com>
2019-06-18 17:37:48 +0200

    Initial project setup

---
+++ src/main/java/com/emramirez/gumballmachine/GumballMachineApplication.java
@@ -0,0 +1,13 @@
+package com.emramirez.gumballmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GumballMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GumballMachineApplication.class, args);
+	}
+
+}
